f10c410ca836ef1731f608d4cce1c123ff46d53b
pr75502 <pp1614582@gmail.com>
2025-11-07 19:57:15 +0530

    add product render issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
 import "./App.css";
 
+import AddProduct from "./components/Products/addProduct.jsx";
 import SignUp from "./components/Auth/signUp";
 import { Toaster } from "react-hot-toast";
 import Login from "./components/Auth/login";
-// import Products from './components/Products/products'
 import Cart from "./components/CartAndCheckout/cart";
 import Checkout from "./components/CartAndCheckout/checkout";
 import ProductDetails from "./components/Products/productDetails";
 import Profile from "./components/User/profile";
-import AddProduct from "./components/Products/addProduct";
+
 import HomePage from "./components/InfoPages/HomePage";
 import SearchResults from "./components/Products/SearchResults";
 import OrderHistory from "./components/User/OrderHistory";
--- src/components/Products/addProduct.jsx
+++ src/components/Products/addProduct.jsx
@@ -6,83 +6,128 @@ import { toast } from "react-hot-toast";
 import { myProducts } from "../../features/productSlice";
 
 const AddProduct = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const nameRef = useRef();
+  const descriptionRef = useRef();
+  const priceRef = useRef();
+  const categoryRef = useRef();
+  const imageUrlRef = useRef();
 
-    const nameRef = useRef()
-    const descriptionRef = useRef()
-    const priceRef = useRef()
-    const categoryRef = useRef()
-    const imageUrlRef = useRef()
+  useEffect(() => {
+    dispatch(myProducts());
+  }, [dispatch]);
 
+  const myProductsData = useSelector((state) => state.product.myProducts);
+  console.log(myProductsData);
 
-    useEffect(() => {
-        dispatch(myProducts())
-    }, [dispatch])
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const myProductsData = useSelector(state => state.product.myProducts)
-    console.log(myProductsData)
+    const formData = new FormData();
+    formData.append("name", nameRef.current.value);
+    formData.append("description", descriptionRef.current.value);
+    formData.append("price", priceRef.current.value);
+    formData.append("category", categoryRef.current.value);
+    formData.append("image", imageUrlRef.current.files[0]);
+    console.log(formData);
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const formData = new FormData();
-        formData.append("name", nameRef.current.value);
-        formData.append("description", descriptionRef.current.value);
-        formData.append("price", priceRef.current.value);
-        formData.append("category", categoryRef.current.value);
-        formData.append("image", imageUrlRef.current.files[0]);
-        console.log(formData)
-
-        try {
-            await dispatch(addProduct(formData)).unwrap();
-            toast.success("Product added successfully");
-            navigate("/homepage");
-            nameRef.current.value = ""
-            descriptionRef.current.value = ""
-            priceRef.current.value = ""
-            categoryRef.current.value = ""
-            imageUrlRef.current.value = ""
-        } catch (error) {
-
-            toast.error(error.message || "Failed to add product");
-        }
+    try {
+      await dispatch(addProduct(formData)).unwrap();
+      toast.success("Product added successfully");
+      navigate("/homepage");
+      nameRef.current.value = "";
+      descriptionRef.current.value = "";
+      priceRef.current.value = "";
+      categoryRef.current.value = "";
+      imageUrlRef.current.value = "";
+    } catch (error) {
+      toast.error(error.message || "Failed to add product");
     }
-    return (
-        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h1 className="text-2xl font-bold text-center text-gray-800 mb-8">Add Product</h1>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                    <input type="text" placeholder="Product Name" ref={nameRef} className="w-full p-2 border border-gray-300 rounded-lg" required />
-                    <input type="text" placeholder="Product Description" ref={descriptionRef} className="w-full p-2 border border-gray-300 rounded-lg" required />
-                    <input type="text" placeholder="Product Price" ref={priceRef} className="w-full p-2 border border-gray-300 rounded-lg" required />
-                    <input type="text" placeholder="Product Category" ref={categoryRef} className="w-full p-2 border border-gray-300 rounded-lg" required />
-                    <input type="file" placeholder="Product Image" ref={imageUrlRef} className="w-full p-2 border border-gray-300 rounded-lg" required />
-                    <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Add Product</button>
-                </form>
+  };
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h1 className="text-2xl font-bold text-center text-gray-800 mb-8">
+          Add Product
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="text"
+            placeholder="Product Name"
+            ref={nameRef}
+            className="w-full p-2 border border-gray-300 rounded-lg"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Product Description"
+            ref={descriptionRef}
+            className="w-full p-2 border border-gray-300 rounded-lg"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Product Price"
+            ref={priceRef}
+            className="w-full p-2 border border-gray-300 rounded-lg"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Product Category"
+            ref={categoryRef}
+            className="w-full p-2 border border-gray-300 rounded-lg"
+            required
+          />
+          <input
+            type="file"
+            placeholder="Product Image"
+            ref={imageUrlRef}
+            className="w-full p-2 border border-gray-300 rounded-lg"
+            required
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+          >
+            Add Product
+          </button>
+        </form>
 
-                <h1 className="text-xl font-bold text-center text-gray-800 my-8">Products Added</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {myProductsData && myProductsData.map((product) => {
-                        return (
-                            <div key={product._id} className="bg-white rounded-lg shadow-lg p-4">
-                                <img src={product.imageUrl} alt={product.name} className="w-full h-48 object-cover rounded-t-lg" />
-                                <div className="p-4">
-                                    <h1 className="text-lg font-bold">{product.name}</h1>
-                                    <p className="text-gray-600">{product.description}</p>
-                                    <p className="text-gray-800 font-bold mt-2">${product.price}</p>
-                                    <p className="text-sm text-gray-500 mt-1">{product.category}</p>
-                                </div>
-                            </div>
-                        )
-                    })
-                    }
+        <h1 className="text-xl font-bold text-center text-gray-800 my-8">
+          Products Added
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {myProductsData &&
+            myProductsData.map((product) => {
+              return (
+                <div
+                  key={product._id}
+                  className="bg-white rounded-lg shadow-lg p-4"
+                >
+                  <img
+                    src={product.imageUrl}
+                    alt={product.name}
+                    className="w-full h-48 object-cover rounded-t-lg"
+                  />
+                  <div className="p-4">
+                    <h1 className="text-lg font-bold">{product.name}</h1>
+                    <p className="text-gray-600">{product.description}</p>
+                    <p className="text-gray-800 font-bold mt-2">
+                      ${product.price}
+                    </p>
+                    <p className="text-sm text-gray-500 mt-1">
+                      {product.category}
+                    </p>
+                  </div>
                 </div>
-
-            </div>
+              );
+            })}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default AddProduct;
